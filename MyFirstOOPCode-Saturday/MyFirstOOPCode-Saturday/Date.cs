f3017c9a6fc876c2937b152bc9a7ebff15249249
FREDY ANTONIO MAZO QUICENO <frema1914@gmail.com>
2023-03-02 14:14:50 -0500

    Date class implemented validation

--- MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Date.cs
+++ MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Date.cs
@@ -4,7 +4,93 @@ using System.Text;
 
 namespace MyFirstOOPCode_Saturday
 {
-    internal class Date
+    public class Date
     {
+        #region Fields
+        
+        private int _year;
+        private int _month;
+        private int _day;
+
+        #endregion
+
+
+        #region Methods
+        //Creacíon método constructor
+        public Date(int year, int month, int day)//parámetros que recibe el constructor
+        {
+            //asignación de los campos que llegan a las variables
+            _year = ValidateYear(year);
+            _month = ValidateMonth(month);//Creación de método ValidateDate que recibe parámetro 
+            _day = ValidateDay(day, month, year);
+        }
+
+        private int ValidateYear(int year)
+        {
+            if (year >= 1900)
+            {
+                return year;
+            }
+
+            throw new YearException("The year is invalid!");
+        }
+
+        private int ValidateMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            
+            throw new DayException("The month is invalid!");
+        }
+
+        private int ValidateDay(int day, int month, int year)
+        {
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+        
+            
+            //Un arreglo siempre tiene la posición cero
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])//Se envía como parámetro el month
+            {
+                return day;
+            }
+            throw new DayException("The day is invalid!");
+                        
+            /*if (month == 2 && day >= 1 && day <= 28)
+            {
+                return day;
+            }
+
+            else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 && day >= 1 && day <= 31)
+            {
+                return day;
+            }
+            else if (day >= 1 && day <= 30)
+            {
+                return day;
+            }*/
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            bool isLeap = (year % 400 == 0 || year % 4 == 0 && year % 100 != 0 );
+            
+            return isLeap;
+        }
+
+        //Creación de método ToString
+        public override string ToString()
+        { 
+            //Interpolación para concatenar $"{}"
+            string date = $"{_year}/{_month}/{_day}";
+            return date;
+        }
+
+        #endregion
     }
 }
--- MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Program.cs
+++ MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Program.cs
@@ -6,7 +6,19 @@ namespace MyFirstOOPCode_Saturday
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola FREDY MAZO");
+            try
+            {
+                //Creación del objeto de la clase date
+                var dateObject = new Date(2024, 2, 29);
+                //Comando para leer
+                Console.WriteLine(dateObject.ToString());
+            }
+            //El cath controla la excepción y recibe la clase ex
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
