b650db849084579858e3f9418a64fea4539646f4
FREDY ANTONIO MAZO QUICENO <frema1914@gmail.com>
2023-03-02 08:22:20 -0500

    Class Date and Class Program created

---
+++ MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Date.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstOOPCode_Saturday
+{
+    internal class Date
+    {
+    }
+}
---
+++ MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstOOPCode_Saturday
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola FREDY MAZO");
+        }
+    }
+}
